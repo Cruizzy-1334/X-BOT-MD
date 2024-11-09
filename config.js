c0a933fa23a4fcd677be55c8f8ab903459300fcb
Cruizzy-1334 <cruizzynaruto@gmail.com>
2024-11-09 10:38:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ if (fs.existsSync('config.env')) {
 const API = "https://api-aswin-sparky.koyeb.app";
 const ALIVE = process.env.ALIVE || "I am Alive";
 const HANDLERS = process.env.HANDLER || ".";
-const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:9474ecc4b958ef807873bebee48a9e34";
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:42026c2cfb9d5dc12faad7358925b90c";
 const SUDO = process.env.SUDO || '917012984396';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "true";
